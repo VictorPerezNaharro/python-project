be9b4b8f79a0b26b2cc705794e32e866fb99c13c
Grester43hck <grester43hck@gmail.com>
2016-08-13 17:07:42 +0200

    first file

--- Python-1.py
+++ Python-1.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-import socket
-
-ip = socket.gethostbyname(socket.gethostname())
-print ip;
+import netifaces as ni
+ni.ifaddresses('wlan0')
+ip = ni.ifaddresses('eth0')[2][0]['addr']
+print ip  # should print "192.168.100.37"
