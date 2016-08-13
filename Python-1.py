f72a6d57f5eddb864c0803c702dbe5c2858e6738
Grester43hck <grester43hck@gmail.com>
2016-08-13 17:29:50 +0200

    first file

--- Python-1.py
+++ Python-1.py
@@ -1,9 +1,17 @@
-import netifaces as ni
-ip = ni.ifaddresses("wlan0")
-print ip  # should print "192.168.100.37"
-ip = ni.ifaddresses("wlan0")[2]
-print ip
-ip = ni.ifaddresses("wlan0")[2][0]
-print ip
-ip = ni.ifaddresses("wlan0")[2][0]['addr']
-print ip
+import socket
+import subprocess
+import tempfile
+import os
+
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+ip = get_ip_address()
+
+p = subprocess.Popen(['ls', '-a'], stdout=subprocess.PIPE, 
+                                 stderr=subprocess.PIPE)
+out, err = p.communicate()
+print out
